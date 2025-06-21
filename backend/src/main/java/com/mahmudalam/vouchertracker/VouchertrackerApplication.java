d8c2bc9a27f882e9fbd66546392372e227ba8d53
Mahmud-Alam <official.mahmud23@gmail.com>
2025-06-21 22:47:25 +0600

    starting a new spring-boot project

---
+++ backend/src/main/java/com/mahmudalam/vouchertracker/VouchertrackerApplication.java
@@ -0,0 +1,13 @@
+package com.mahmudalam.vouchertracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VouchertrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VouchertrackerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mahmudalam/vouchertracker/VouchertrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahmudalam.vouchertracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VouchertrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
